6265d117aed6887131573ea74ef250f5c3e70477
chrisTanYK <christan_yaokun82@hotmail.com>
2025-05-20 18:01:19 +0800

    Initial commit for ECS X-Ray project

---
+++ ecs.tf
@@ -0,0 +1,196 @@
+# ECR Repository
+resource "aws_ecr_repository" "flask_xray" {
+  name = "${var.prefix}-flask-xray-repo"
+}
+
+# SSM Parameter
+resource "aws_ssm_parameter" "app_config" {
+  name  = "/${var.prefix}/config"
+  type  = "String"
+  value = "MySSMConfig"
+}
+
+# Secrets Manager Secret
+resource "aws_secretsmanager_secret" "db_password" {
+  name = "${var.prefix}/db_password"
+}
+
+resource "aws_secretsmanager_secret_version" "db_password" {
+  secret_id     = aws_secretsmanager_secret.db_password.id
+  secret_string = "MySecretPassword123!"
+}
+
+# Task Role
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.prefix}-ecs-xray-taskrole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "xray_write" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+}
+
+# Task Execution Role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.prefix}-ecs-xray-taskexecutionrole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_read" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "secrets_manager" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ECS Cluster
+resource "aws_ecs_cluster" "flask_xray" {
+  name = "${var.prefix}-flask-xray-cluster"
+}
+
+# Task Definition
+resource "aws_ecs_task_definition" "flask_xray" {
+  family                   = "${var.prefix}-flask-xray-taskdef"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "flask-app"
+      image     = "${aws_ecr_repository.flask_xray.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name  = "SERVICE_NAME"
+          value = var.service_name
+        }
+      ]
+      secrets = [
+        {
+          name      = "MY_APP_CONFIG"
+          valueFrom = aws_ssm_parameter.app_config.arn
+        },
+        {
+          name      = "MY_DB_PASSWORD"
+          valueFrom = aws_secretsmanager_secret.db_password.arn
+        }
+      ]
+    },
+    {
+      name      = "xray-sidecar"
+      image     = "amazon/aws-xray-daemon"
+      essential = false
+      portMappings = [
+        {
+          containerPort = 2000
+          hostPort      = 2000
+          protocol      = "udp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/${var.prefix}-flask-xray"
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+# ECS Service
+resource "aws_ecs_service" "flask_xray" {
+  name            = "${var.prefix}-flask-service"
+  cluster         = aws_ecs_cluster.flask_xray.id
+  task_definition = aws_ecs_task_definition.flask_xray.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = data.aws_subnets.public.ids
+    security_groups  = [aws_security_group.ecs.id]
+    assign_public_ip = true
+  }
+}
+
+# Security Group
+resource "aws_security_group" "ecs" {
+  name        = "${var.prefix}-ecs-sg"
+  description = "Allow inbound access to ECS service"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Data sources
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "map-public-ip-on-launch"
+    values = ["true"]
+  }
+}
